92481bb87df4ad0b76f793094d1d835c15bfffc4
benjamin mcmichael <benjamin mcmichael>
2016-09-14 13:03:42 -0400

    Ben: client can recieve the message history and reverse it into a String

---
+++ src/main/java/tiy/webapp/Client.java
@@ -0,0 +1,56 @@
+package tiy.webapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+     * Created by fenji on 9/13/2016.
+     */
+public class Client {
+
+    static final String HOST_ADDRESS = "localhost";
+    static final int PORT_NUMBER = 8088;
+    PrintWriter out = null;
+    BufferedReader in = null;
+
+
+    public String sendMessage(String message){
+        String serverResponse = null;
+        ArrayList<String> messageHistory = null;
+        try {
+            Socket clientSocket = new Socket(HOST_ADDRESS, PORT_NUMBER);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println(message);
+            //get initial response
+            serverResponse = in.readLine();
+
+            //loop to accept the entire chat histry into a String Arraylist
+            messageHistory = new ArrayList<String>();
+            while (!serverResponse.equalsIgnoreCase("end:history")){
+                messageHistory.add(serverResponse);
+                serverResponse = in.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //reverse the arrayList into a String with a newline between each message
+        StringBuilder historyBuilder = new StringBuilder();
+        for (int count = messageHistory.size(); count > 0; count--){
+            if (count == 1){
+                historyBuilder.append(messageHistory.get(count - 1).toCharArray());
+            } else{
+                historyBuilder.append(messageHistory.get(count - 1).toCharArray());
+                historyBuilder.append('\n');
+            }
+        }
+        String chatHistory = historyBuilder.toString();
+        return chatHistory;
+    }
+}
