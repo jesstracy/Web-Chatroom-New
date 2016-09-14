5b3a3dd5370334a87bb66c3d14ab5d413de3e410
Jessica Tracy <j.tracy916@gmail.com>
2016-09-14 13:47:28 -0400

    Made login endpoint, added controller annotation

--- src/main/java/tiy/webapp/ChatSpringAppController.java
+++ src/main/java/tiy/webapp/ChatSpringAppController.java
@@ -1,5 +1,6 @@
 package tiy.webapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,33 +10,25 @@ import javax.servlet.http.HttpSession;
 /**
  * Created by jessicatracy on 9/14/16.
  */
+@Controller
 public class ChatSpringAppController {
-    //    Client myClient = new Client();
+//        Client myClient = new Client();
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
 
-//    @RequestMapping(path = "/chat", method = RequestMethod.GET)
-//    public String chat() {
-////        myClient.sendMessage(message);
-//
-//        return "input";
-//    }
-
-
-    @RequestMapping(path = "/chatNew", method = RequestMethod.GET)
-    public String inputNew(Model model, HttpSession session, String message) {
+    @RequestMapping(path = "/chat", method = RequestMethod.GET)
+    public String input(String message) {
         if (message != null) {
-            //When I'm just sysouting the server response, I don't need to set the message on the session - > just need it to send to server and can go away each time!
-            //Also don't need to add to model because the view does not need to see it at all.
-
-//            myWebChatClient = new WebChatClient();
-//            String serverResponse = myWebChatClient.sendMessage(message);
-//            System.out.println(serverResponse);
-
-            //BUT, I will have to add it to the model when I'm printing it to the browser screen!
-//            model.addAttribute("serverResponse", serverResponse);
-
+//        myClient.sendMessage(message); <-- returns string, entire history, newest message on top
         }
 
         return "input";
     }
+
+
+
 }
