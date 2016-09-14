4ae58c5a6e4d21c6ef97c6b654465cb1ec7b8203
Brett Gordon <gordonbrett89@gmail.com>
2016-09-14 14:20:34 -0400

    New database funcionality

--- src/test/java/tiy/webapp/ChatDatabaseTest.java
+++ src/test/java/tiy/webapp/ChatDatabaseTest.java
@@ -31,64 +31,34 @@ public class ChatDatabaseTest {
 	public void tearDown() throws Exception {
 
 	}
-
-	@Test
-	public void insertUser() throws Exception {
-		Connection conn = DriverManager.getConnection(testDatabase.DB_URL);
-		String fullName = "Brett Gordon";
-		String userName = "theNinjew";
-
-		int userID = testDatabase.insertUser(conn, fullName, userName);
-
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
-		stmt.setInt(1, userID);
-		ResultSet result = stmt.executeQuery();
-		result.next();
-		assertNotNull(result.getString("username"));
-
-		testDatabase.deleteUser(conn, userName);
-
-	}
-
 	@Test
 	public void addMessage() throws Exception {
 		Connection conn = DriverManager.getConnection(testDatabase.DB_URL);
-		String fullName = "Brett Gordon";
-		String userName = "theNinjew";
-
-		int userID = testDatabase.insertUser(conn, fullName, userName);
 
 		String message = "This is a test message, we should never, EVER see it.";
 
-		testDatabase.postMessage(conn, message, userID);
+		testDatabase.postMessage(conn, message);
 
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages WHERE user_id = ?");
-		stmt.setInt(1, userID);
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages where id = ?");
+		stmt.setInt(1, 1);
 		ResultSet result = stmt.executeQuery();
 		result.next();
 		assertEquals("This is a test message, we should never, EVER see it.", result.getString("message"));
 
 		testDatabase.deleteMessage(conn, message);
 
-		testDatabase.deleteUser(conn, userName);
-
-
 	}
 
 	@Test
 	public void history() throws Exception {
 		Connection conn = DriverManager.getConnection(testDatabase.DB_URL);
-		String fullName = "Brett Gordon";
-		String userName = "theNinjew";
-
-		int userID = testDatabase.insertUser(conn, fullName, userName);
 
 		String message = "This is a test message, we should never, EVER see it.";
-		testDatabase.postMessage(conn, message, userID);
+		testDatabase.postMessage(conn, message);
 		message = "This is another test message, we want to see this.";
-		testDatabase.postMessage(conn, message, userID);
+		testDatabase.postMessage(conn, message);
 		message = "And we want to see ALL of them.";
-		testDatabase.postMessage(conn, message, userID);
+		testDatabase.postMessage(conn, message);
 
 		ArrayList<String> testArray = testDatabase.chatHistory(conn);
 
@@ -99,8 +69,6 @@ public class ChatDatabaseTest {
 		assertEquals(3, testArray.size());
 
 		testDatabase.deleteMessage(conn, message);
-
-		testDatabase.deleteUser(conn, userName);
 	}
 
 }
