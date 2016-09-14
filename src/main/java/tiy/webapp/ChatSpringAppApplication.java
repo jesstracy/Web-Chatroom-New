efa1b5289285822617d5eadd017e8a50aa417c8b
Jessica Tracy <j.tracy916@gmail.com>
2016-09-14 12:04:24 -0400

    Initial commit

---
+++ src/main/java/tiy/webapp/ChatSpringAppApplication.java
@@ -0,0 +1,12 @@
+package tiy.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSpringAppApplication.class, args);
+	}
+}
