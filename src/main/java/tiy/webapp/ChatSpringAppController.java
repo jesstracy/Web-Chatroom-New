870d85e1f388f7bf211f2e67a92e370d45a85f74
Jessica Tracy <j.tracy916@gmail.com>
2016-09-14 14:45:08 -0400

    Storing username, printing test array

--- src/main/java/tiy/webapp/ChatSpringAppController.java
+++ src/main/java/tiy/webapp/ChatSpringAppController.java
@@ -1,11 +1,13 @@
 package tiy.webapp;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by jessicatracy on 9/14/16.
@@ -13,17 +15,29 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class ChatSpringAppController {
 //        Client myClient = new Client();
+    String username = "";
 
     @RequestMapping(path = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(Model model, HttpSession session, String username) {
         return "login";
+    }
+
+    @RequestMapping(path = "/get_username", method = RequestMethod.POST)
+    public String get_username(String username) {
+        this.username = username;
+        System.out.println(this.username);
+        return "redirect:/chat";
     }
 
 
     @RequestMapping(path = "/chat", method = RequestMethod.GET)
-    public String input(String message) {
+    public String input(HttpSession session, Model model, String message) {
         if (message != null) {
-//        myClient.sendMessage(message); <-- returns string, entire history, newest message on top
+//        String serverResponse = myClient.sendMessage(session.getAttribute("username"), message); <-- returns string, entire history, newest message on top
+            System.out.println("Username is: " + session.getAttribute("username"));
+            String serverResponse = "test 1\ntest 2\ntest 3\ntest4";
+            String[] serverMessages = serverResponse.split("\n");
+            model.addAttribute("serverMessages", serverMessages);
         }
 
         return "input";
